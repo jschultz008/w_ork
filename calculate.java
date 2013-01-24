be9e115b5ad68687d739263cddc16a90656a60df
Joseph <JSchultz008@gmail.com>
2013-01-23 20:57:22 -0800

    Turns out I messed up!
    TAKE TWO!

---
+++ calculate.java
@@ -0,0 +1,10 @@
+
+public class calculate {
+	public String calculateWork(float f, float d){
+		return toString((f * d));
+	}
+	
+	public String toString(Float answer){
+		return "W = " + answer + " joules";
+}
+}
--- frontend.java
+++ frontend.java
@@ -1,16 +1,10 @@
-/**
- * A Simple Project : Force of Gravity.
- * Given G, Mass of Object One, Mass of Object 2, and Distance (r) : Return F (Newtons)
- * @author Joseph
- * This project makes little attempt at efficiency, opting instead for a 'quick and dirty' Calculation without being hung up on Design Patterning.
- */
-public class frontend{
-	
+
+public class frontend {
+
 	public static void main(String args[]){
+		calculate c = new calculate();
 		prompt p = new prompt();
-		calculator c = new calculator();
-			
-		System.out.println( c.toString(c.calculate(p.askG(), p.askM1(), p.askM2(), p.askR() ))); // though this does look like the flywheel :P
 		
+		System.out.println( c.calculateWork(p.askF(), p.askD()) );
 	}
 }
